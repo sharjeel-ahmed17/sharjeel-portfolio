aff71031a4620569a159a7b8b9353a87814a560d
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-05-22 02:21:24 +0500

    add

---
+++ src/components/data/Data.jsx
@@ -0,0 +1,36 @@
+const workData = [
+    {
+        title: "Project Alpha",
+        description: "A comprehensive project that explores advanced AI techniques and their applications in various fields.",
+        imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXs8fVridzoME5y2GR5TVbjk8kpMeY_z0CFMCX98TuZQ&s",
+        link: "https://example.com/",
+        btnText: "visit site",
+        alt: "this is some how"
+    },
+    {
+        title: "Project Beta",
+        description: "An innovative project focused on the development of sustainable energy solutions.",
+        imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXs8fVridzoME5y2GR5TVbjk8kpMeY_z0CFMCX98TuZQ&s",
+        link: "https://example.com/",
+        btnText: "visit site",
+        alt: "this is some how"
+    },
+    {
+        title: "Project Gamma",
+        description: "A collaborative project aiming to enhance data security through cutting-edge cryptographic methods.",
+        imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXs8fVridzoME5y2GR5TVbjk8kpMeY_z0CFMCX98TuZQ&s",
+        link: "https://example.com/project-gamma",
+        btnText: "visit site",
+        alt: "this is some how"
+    },
+    {
+        title: "Project Delta",
+        description: "An exciting project dedicated to creating immersive virtual reality experiences.",
+        imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXs8fVridzoME5y2GR5TVbjk8kpMeY_z0CFMCX98TuZQ&s",
+        link: "https://example.com/",
+        btnText: "visit site",
+        alt: "this is some how"
+    }
+];
+
+export default workData;
