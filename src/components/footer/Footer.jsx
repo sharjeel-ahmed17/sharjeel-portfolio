e5624879418262fbe29a928ae68634b40151d6bb
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-05-22 12:04:25 +0500

    update-footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,17 +1,13 @@
 
 import FooterAboveMenu from './FooterAboveMenu'
+import FooterBelow from './FooterBelow'
 
 const Footer = () => {
     return (
         <>
             <FooterAboveMenu />
 
-            <div className='bg-primary'>
-                <div className="container">
-                    <footer className='py-4 text-center text-white'>All Right Reversed By Sharjeel Ahmed 2024</footer>
-                </div>
-
-            </div>
+            <FooterBelow />
         </>
     )
 }
---
+++ src/components/footer/FooterBelow.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterBelow = () => {
+    return (
+        <div className='bg-primary'>
+            <div className="container">
+                <footer className='py-4 text-center text-white'>All Right Reversed By Sharjeel Ahmed 2024</footer>
+            </div>
+
+        </div>
+    )
+}
+
+export default FooterBelow
